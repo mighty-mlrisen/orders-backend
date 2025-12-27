737265c4d850fb6a58455725a2eedeac8c90002e
Artyom Mazurenko <artemmazurenko71@gmail.com>
2025-12-27 14:06:01 +0300

    fix

--- backend/BackendApp/Consumer/obj/Debug/net9.0/Consumer.AssemblyInfo.cs
+++ backend/BackendApp/Consumer/obj/Debug/net9.0/Consumer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Consumer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+149cc670158074c46861e512a85843d299a4fd03")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f92ef8c8ecfb97f35de44cc27fc350c0ee7085d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Consumer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Consumer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
