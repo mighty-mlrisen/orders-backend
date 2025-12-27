2f92ef8c8ecfb97f35de44cc27fc350c0ee7085d
Artyom Mazurenko <artemmazurenko71@gmail.com>
2025-12-27 09:07:27 +0300

    fix lab3

--- backend/BackendApp/Models/obj/Debug/net9.0/Models.AssemblyInfo.cs
+++ backend/BackendApp/Models/obj/Debug/net9.0/Models.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Models")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b3807460e8782680f9616d296c892cb098804f8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+149cc670158074c46861e512a85843d299a4fd03")]
 [assembly: System.Reflection.AssemblyProductAttribute("Models")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Models")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
